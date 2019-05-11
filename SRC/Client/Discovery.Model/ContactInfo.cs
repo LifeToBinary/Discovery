47017080f99dc3d924a933dd43d817b76299acbd
ToBinary <ToBinary@outlook.com>
2019-05-12 00:53:08 +0800

    添加了Discovery.Model.ContactInfo 类

---
+++ SRC/Client/Discovery.Model/ContactInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace Discovery.Model
+{
+    public class ContactInfo : BindableBase
+    {
+        private string _qq;
+        public string QQ
+        {
+            get => _qq;
+            set => SetProperty(ref _qq, value);
+        }
+        private string _weChat;
+        public string WeChat
+        {
+            get => _weChat;
+            set => SetProperty(ref _weChat, value);
+        }
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+        public string _blogAddress;
+        public string BlogAddress
+        {
+            get => _blogAddress;
+            set => SetProperty(ref _blogAddress, value);
+        }
+    }
+}
