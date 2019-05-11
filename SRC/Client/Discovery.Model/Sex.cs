5b5836f74cada97f6f28b8b078554975b35844f3
ToBinary <ToBinary@outlook.com>
2019-05-12 01:04:32 +0800

    添加了Discovery.Model.Sex 枚举类

---
+++ SRC/Client/Discovery.Model/Sex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discovery.Model
+{
+    /// <summary>
+    /// 表示一个性别
+    /// </summary>
+    public enum Sex
+    {
+        /// <summary>
+        /// 男
+        /// </summary>
+        Male,
+        /// <summary>
+        /// 女
+        /// </summary>
+        Female
+    }
+}
