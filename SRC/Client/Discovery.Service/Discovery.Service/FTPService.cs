84e80f70bcdbc26b1904582f0a654665e5fc9d25
ToBinary <ToBinary@outlook.com>
2019-05-11 20:40:09 +0800

    对FTPService类的现有代码添加了注释

--- SRC/Client/Discovery.Service/Discovery.Service/FTPService.cs
+++ SRC/Client/Discovery.Service/Discovery.Service/FTPService.cs
@@ -6,7 +6,15 @@ namespace Discovery.Service
 {
     public class FTPService
     {
+        /// <summary>
+        /// FTP 服务器完整路径
+        /// </summary>
         private readonly string _ftpServiceAddress;
+
+        /// <summary>
+        /// 实例化一个FTPService 对象
+        /// </summary>
+        /// <param name="ftpServiceAddress"> FTP 服务器完整路径</param>
         public FTPService(string ftpServiceAddress)
             => _ftpServiceAddress = ftpServiceAddress;
     }
