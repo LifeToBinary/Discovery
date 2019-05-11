558e5c9c5af5aac5d212f2db535aa83a5359cded
ToBinary <ToBinary@outlook.com>
2019-05-11 20:19:20 +0800

    添加了Discovery.Service.FTPServiceTest.MakeNewFTPService 测试辅助方法

---
+++ TEST/IntegrationTest/Discovery.Service.UnitTest/Discovery.Service.Test/FTPServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Discovery.Service.Test
+{
+    [TestFixture]
+    public class FTPServiceTests
+    {
+        private FTPService MakeNewFTPService()
+            => new FTPService(string ftpAddress, string ftpRemotePath);
+    }
+}
