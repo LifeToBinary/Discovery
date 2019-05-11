d478741a9c5459302cba37db2c701d53fff242a5
ToBinary <ToBinary@outlook.com>
2019-05-12 01:07:33 +0800

    添加了Discovery.Model.Discoverer类

---
+++ SRC/Client/Discovery.Model/Discoverer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace Discovery.Model
+{
+    public class Discoverer : BindableBase
+    {
+        private BasicInfo _basicInfo;
+        public BasicInfo BasicInfo
+        {
+            get => _basicInfo;
+            set => SetProperty(ref _basicInfo, value);
+        }
+
+        private ContactInfo _contactInfo;
+        public ContactInfo ContactInfo
+        {
+            get => _contactInfo;
+            set => SetProperty(ref _contactInfo, value);
+        }
+    }
+}
