b569142ea69e18c09405ceba5b9f21ffabe05d4d
ToBinary <ToBinary@outlook.com>
2019-05-12 01:13:19 +0800

    为Discoverer类的现有代码添加了注释

--- SRC/Client/Discovery.Model/Discoverer.cs
+++ SRC/Client/Discovery.Model/Discoverer.cs
@@ -5,8 +5,14 @@ using Prism.Mvvm;
 
 namespace Discovery.Model
 {
+    /// <summary>
+    /// 用户类
+    /// </summary>
     public class Discoverer : BindableBase
     {
+        /// <summary>
+        /// 基本信息
+        /// </summary>
         private BasicInfo _basicInfo;
         public BasicInfo BasicInfo
         {
@@ -14,6 +20,9 @@ namespace Discovery.Model
             set => SetProperty(ref _basicInfo, value);
         }
 
+        /// <summary>
+        /// 联系方式
+        /// </summary>
         private ContactInfo _contactInfo;
         public ContactInfo ContactInfo
         {
