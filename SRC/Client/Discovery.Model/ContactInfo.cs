29b8325de0d86adc9998b495985904e39a88cac9
ToBinary <ToBinary@outlook.com>
2019-05-12 01:12:07 +0800

    为ContactInfo类的现有代码添加了注释

--- SRC/Client/Discovery.Model/ContactInfo.cs
+++ SRC/Client/Discovery.Model/ContactInfo.cs
@@ -5,26 +5,44 @@ using Prism.Mvvm;
 
 namespace Discovery.Model
 {
+    /// <summary>
+    /// 表示用户的联系方式
+    /// </summary>
     public class ContactInfo : BindableBase
     {
+        /// <summary>
+        /// QQ 号
+        /// </summary>
         private string _qq;
         public string QQ
         {
             get => _qq;
             set => SetProperty(ref _qq, value);
         }
+
+        /// <summary>
+        /// 微信号
+        /// </summary>
         private string _weChat;
         public string WeChat
         {
             get => _weChat;
             set => SetProperty(ref _weChat, value);
         }
+
+        /// <summary>
+        /// 邮箱
+        /// </summary>
         private string _email;
         public string Email
         {
             get => _email;
             set => SetProperty(ref _email, value);
         }
+
+        /// <summary>
+        /// 博客地址
+        /// </summary>
         public string _blogAddress;
         public string BlogAddress
         {
