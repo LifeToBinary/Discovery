a1b515244452105de7b5e4981729945e2715e1ee
ToBinary <ToBinary@outlook.com>
2019-05-15 23:13:44 +0800

    将MainWindow移动到Views文件夹

--- SRC/Client/Discovery.Client/Views/MainWindow.xaml.cs
+++ SRC/Client/Discovery.Client/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Discovery.Client
+namespace Discovery.Client.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
