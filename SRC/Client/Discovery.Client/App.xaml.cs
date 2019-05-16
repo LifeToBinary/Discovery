bca50a519ba3772d7d265db13e981c4d734715c0
ToBinary <ToBinary@outlook.com>
2019-05-16 18:54:41 +0800

    修复了 Unity.Abstractions 无法加载的问题

--- SRC/Client/Discovery.Client/App.xaml.cs
+++ SRC/Client/Discovery.Client/App.xaml.cs
@@ -1,35 +1,26 @@
 ﻿using Discovery.Client.Views;
-using Prism.Unity;
-using System.Windows;
 using Prism.Ioc;
-using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Discovery.Client
 {
     /// <summary>
-    /// 应用程序启动类
+    /// App.xaml 的交互逻
     /// </summary>
     public partial class App : PrismApplication
     {
-        /// <summary>
-        /// 设置启动窗口
-        /// </summary>
-        /// <returns></returns>
         protected override Window CreateShell()
             => Container.Resolve<MainWindow>();
 
-        /// <summary>
-        /// 注册类型
-        /// </summary>
-        /// <param name="containerRegistry"></param>
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-
-        }
-
-        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
-        {
-
         }
     }
 }
