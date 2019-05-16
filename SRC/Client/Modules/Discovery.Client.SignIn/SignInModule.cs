25a15943d86683a15e8fd1100ccd1595f1489bd7
ToBinary <ToBinary@outlook.com>
2019-05-17 01:45:40 +0800

    添加此模块的基本文件和代码

---
+++ SRC/Client/Modules/Discovery.Client.SignIn/SignInModule.cs
@@ -0,0 +1,22 @@
+﻿using Discovery.Core.Constants;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.SignIn
+{
+    public class SignInModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+            => containerProvider.Resolve<IRegionManager>()
+                                .RegisterViewWithRegion(RegionNames.MainRegion, typeof(Views.SignIn));
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+            => containerRegistry.RegisterForNavigation<Views.SignIn>();
+    }
+}
