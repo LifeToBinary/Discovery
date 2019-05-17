0354a98c603bc0b0f6e975b0b89e58226353736e
ToBinary <ToBinary@outlook.com>
2019-05-17 23:27:36 +0800

    添加 Discovery.Client.Feedback 模块的基本文件和代码

---
+++ SRC/Client/Modules/Discovery.Client.Feedback/FeedbackModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.Feedback
+{
+    public class FeedbackModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+            => containerRegistry.RegisterForNavigation<Views.Feedback>();
+    }
+}
