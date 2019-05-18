6eb81379e0e291d4d2a56d5ea7c827e13d4e6a61
ToBinary <ToBinary@outlook.com>
2019-05-19 00:08:50 +0800

    测试: AuthenticationCodeBuilder.Build 生成的验证码的长度是否正确

---
+++ TEST/UnitTest/Discovery.Core.UnitTest/AuthenticationCodeBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Core.UnitTest
+{
+    [TestFixture]
+    public class AuthenticationCodeBuilderTests
+    {
+        private AuthenticationCodeBuilder MakeAuthenticationCodeBuilder()
+            => new AuthenticationCodeBuilder();
+        [Test]
+        public void Build_CountIs4_ResultCountIs4()
+        {
+            AuthenticationCodeBuilder builder = MakeAuthenticationCodeBuilder();
+            builder.CodeCount = 4;
+            int expected = 4;
+
+            int actual = builder.Build().Length;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
