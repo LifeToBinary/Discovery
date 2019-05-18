5b12bc17fa243a5ef8b0596587d64af57f320cd8
ToBinary <ToBinary@outlook.com>
2019-05-19 02:27:53 +0800

    为AuthenticationCodeBuilder类的现有代码添加了注释

--- SRC/Client/Discovery.Core/Tools/AuthenticationCodeBuilder.cs
+++ SRC/Client/Discovery.Core/Tools/AuthenticationCodeBuilder.cs
@@ -4,12 +4,31 @@ using System.Text;
 
 namespace Discovery.Core.Tools
 {
+    /// <summary>
+    /// 验证码生成器
+    /// </summary>
     public class AuthenticationCodeBuilder
     {
+        /// <summary>
+        /// 验证码长度
+        /// </summary>
         public int CodeLength { get; set; }
+
+        /// <summary>
+        /// 验证码字符范围
+        /// </summary>
         public string CharsSource { get; set; }
+
+        /// <summary>
+        /// 随机器
+        /// </summary>
         private readonly Random _random = 
             new Random(DateTime.Now.Ticks.GetHashCode());
+
+        /// <summary>
+        /// 生成验证码
+        /// </summary>
+        /// <returns>验证码</returns>
         public string Build()
         {
             var result = new StringBuilder();
