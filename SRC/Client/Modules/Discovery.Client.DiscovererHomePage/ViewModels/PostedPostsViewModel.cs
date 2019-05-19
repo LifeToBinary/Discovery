c37536750715a51a6aed02ea7dbc1d1bff4b3b64
ToBinary <ToBinary@outlook.com>
2019-05-19 23:25:05 +0800

    个人主页中的'我的帖子(我发布的)' 板块的基本功能实现

--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/PostedPostsViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/PostedPostsViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,26 @@ namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
     public class PostedPostsViewModel : BindableBase
     {
+        public Discoverer CurrentUser { get; }
+        private ObservableCollection<Post> _postsIPost;
+        public ObservableCollection<Post> PostsIPost
+        {
+            get => _postsIPost;
+            set => SetProperty(ref _postsIPost, value);
+        }
+
+        public PostedPostsViewModel()
+        {
+            CurrentUser = GlobalObjectHolder.CurrentUser;
+            _postsIPost = new ObservableCollection<Post>(LoadData());
+        }
+
+        private Post[] LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                return databaseService.GetPostsOfTheDiscoverer(CurrentUser.BasicInfo.ID);
+            }
+        }
     }
 }
