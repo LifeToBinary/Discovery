e14fe94f8fe1d4a7e0880c20c6b2d408cf1b6926
ToBinary <ToBinary@outlook.com>
2019-05-20 00:17:12 +0800

    个人主页中的'我关注的人'板块的基本功能实现

--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyConcernViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyConcernViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,24 @@ namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
     public class MyConcernViewModel : BindableBase
     {
+        public Discoverer CurrentUser { get; }
+        private ObservableCollection<Discoverer> _idols;
+        public ObservableCollection<Discoverer> Idols
+        {
+            get => _idols;
+            set => SetProperty(ref _idols, value);
+        }
+        public MyConcernViewModel()
+        {
+            CurrentUser = GlobalObjectHolder.CurrentUser;
+            _idols = new ObservableCollection<Discoverer>(LoadData());
+        }
+        private Discoverer[] LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                return databaseService.GetIdols(CurrentUser.BasicInfo.ID);
+            }
+        }
     }
 }
