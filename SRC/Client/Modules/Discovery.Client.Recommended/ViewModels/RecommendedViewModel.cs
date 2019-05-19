f46e01beb11761a3cccbed22b79a789396a6b749
ToBinary <ToBinary@outlook.com>
2019-05-20 02:06:48 +0800

    推荐模块的基本功能实现

--- SRC/Client/Modules/Discovery.Client.Recommended/ViewModels/RecommendedViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.Recommended/ViewModels/RecommendedViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.Recommended.DataBaseService;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,23 @@ namespace Discovery.Client.Recommended.ViewModels
 {
     public class RecommendedViewModel : BindableBase
     {
+        private ObservableCollection<Post> _recommendedPosts;
+        public ObservableCollection<Post> RecommendedPosts
+        {
+            get => _recommendedPosts;
+            set => SetProperty(ref _recommendedPosts, value);
+        }
+        public RecommendedViewModel()
+        {
+            _recommendedPosts = new ObservableCollection<Post>(LoadData());
+        }
+        private Post[] LoadData()
+        {
+            int currentUserID = GlobalObjectHolder.CurrentUser.BasicInfo.ID;
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                return databaseService.GetRecommendedForTheDiscoverer(currentUserID);
+            }
+        }
     }
 }
