25cb44c0fd9c9fb5558cd9650249b52329ecc0b2
ToBinary <ToBinary@outlook.com>
2019-05-20 00:01:19 +0800

    个人主页中的 '我的收藏' 板块的基本功能实现

--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/FavoritedPostsViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/FavoritedPostsViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,26 @@ namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
     public class FavoritedPostsViewModel : BindableBase
     {
+        public Discoverer CurrentUser { get; }
+        private ObservableCollection<Post> _myFavorites;
+        public ObservableCollection<Post> MyFavorites
+        {
+            get => _myFavorites;
+            set => SetProperty(ref _myFavorites, value);
+        }
+
+        public FavoritedPostsViewModel()
+        {
+            CurrentUser = GlobalObjectHolder.CurrentUser;
+            _myFavorites = new ObservableCollection<Post>(LoadData());
+        }
+
+        private Post[] LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                return databaseService.GetFavoritePosts(CurrentUser.BasicInfo.ID);
+            }
+        }
     }
 }
