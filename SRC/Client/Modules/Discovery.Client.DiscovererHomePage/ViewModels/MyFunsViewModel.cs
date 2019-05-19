c0a2741e5a49d07630b5e432d857493941f707a6
ToBinary <ToBinary@outlook.com>
2019-05-20 01:48:00 +0800

    个人主页中的'关注我的人' 板块的基本功能实现

--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyFunsViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyFunsViewModel.cs
@@ -1,6 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,24 @@ namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
     public class MyFunsViewModel : BindableBase
     {
+        public Discoverer CurrentUser { get; }
+        private ObservableCollection<Discoverer> _funs;
+        public ObservableCollection<Discoverer> Funs
+        {
+            get => _funs;
+            set => SetProperty(ref _funs, value);
+        }
+        public MyFunsViewModel()
+        {
+            CurrentUser = GlobalObjectHolder.CurrentUser;
+            Funs = new ObservableCollection<Discoverer>(LoadData());
+        }
+        private Discoverer[] LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                return databaseService.GetFunsOfTheIdol(CurrentUser.BasicInfo.ID);
+            }
+        }
     }
 }
