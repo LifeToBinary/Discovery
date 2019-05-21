dc9d98ebc13c682c8ba2388e40d0083296b0a182
ToBinary <ToBinary@outlook.com>
2019-05-22 03:39:51 +0800

    用户主页的四个板块 改为使用 TabControl

--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyHomePageViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyHomePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Discovery.Core.Constants;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.Constants;
 using Discovery.Core.GlobalData;
 using Discovery.Core.Model;
 using Prism.Commands;
@@ -8,25 +9,101 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
-    public class MyHomePageViewModel : BindableBase
+    public class MyHomePageViewModel : BindableBase, IRegionMemberLifetime
     {
         public Discoverer CurrentUser { get; }
         private readonly IRegionManager _regionManager;
+        private ObservableCollection<Post> _postsIPost;
+        public ObservableCollection<Post> PostsIPost
+        {
+            get => _postsIPost;
+            set => SetProperty(ref _postsIPost, value);
+        }
+        private ObservableCollection<Discoverer> _idols;
+        public ObservableCollection<Discoverer> Idols
+        {
+            get => _idols;
+            set => SetProperty(ref _idols, value);
+        }
+        private ObservableCollection<Post> _myFavorites;
+        public ObservableCollection<Post> MyFavorites
+        {
+            get => _myFavorites;
+            set => SetProperty(ref _myFavorites, value);
+        }
+        private ObservableCollection<Discoverer> _funs;
+        public ObservableCollection<Discoverer> Funs
+        {
+            get => _funs;
+            set => SetProperty(ref _funs, value);
+        }
         public MyHomePageViewModel(IRegionManager regionManager)
         {
-            _regionManager = regionManager;
-            NavigationToProfileContentRegionCommand =
-                new DelegateCommand<string>(NavigationToProfileContentRegion);
             CurrentUser = GlobalObjectHolder.CurrentUser;
+            _regionManager = regionManager;
+            ViewPostDetailCommand = new DelegateCommand<Post>(ViewPostDetail);
+            ViewMyPostDetailCommand = new DelegateCommand<Post>(ViewMyPostDetail);
+            ViewThisUsersHomePageCommand = new DelegateCommand<Discoverer>(ViewThisUsersHomePage);
+            LoadData();
         }
-        public DelegateCommand<string> NavigationToProfileContentRegionCommand { get; }
-        private void NavigationToProfileContentRegion(string viewName)
-            => _regionManager.RequestNavigate(RegionNames.ProfileContent, viewName);
+        private void LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                PostsIPost = 
+                    new ObservableCollection<Post>(
+                        databaseService.GetPostsOfTheDiscoverer(
+                            CurrentUser.BasicInfo.ID));
+                Idols =
+                    new ObservableCollection<Discoverer>(
+                        databaseService.GetIdols(
+                            CurrentUser.BasicInfo.ID));
+
+                MyFavorites =
+                    new ObservableCollection<Post>(
+                        databaseService.GetFavoritePosts(
+                            CurrentUser.BasicInfo.ID));
+
+                Funs =
+                    new ObservableCollection<Discoverer>(
+                        databaseService.GetFunsOfTheIdol(
+                            CurrentUser.BasicInfo.ID));
+            }
+        }
+        public DelegateCommand<Post> ViewPostDetailCommand { get; }
+        private void ViewPostDetail(Post post)
+            => _regionManager.RequestNavigate(
+                                  RegionNames.MainMenuContent,
+                                  ViewNames.OtherUsersPostDetail,
+                                  new NavigationParameters
+                                  {
+                                      { "Post", post }
+                                  });
+        public DelegateCommand<Discoverer> ViewThisUsersHomePageCommand { get; }
+        private void ViewThisUsersHomePage(Discoverer discoverer)
+            => _regionManager.RequestNavigate(
+                                  RegionNames.MainMenuContent,
+                                  ViewNames.OtherUsersHomePage,
+                                  new NavigationParameters
+                                  {
+                                      { "Discoverer", discoverer }
+                                  });
+        public DelegateCommand<Post> ViewMyPostDetailCommand { get; }
+        private void ViewMyPostDetail(Post post)
+            => _regionManager.RequestNavigate(
+                          RegionNames.MainMenuContent,
+                          ViewNames.MyPostDetail,
+                          new NavigationParameters
+                          {
+                              { "Post", post }
+                          });
+        public bool KeepAlive => false;
     }
 }
--- SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/OtherUsersHomePageViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/OtherUsersHomePageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Discovery.Core.Constants;
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.Constants;
 using Discovery.Core.GlobalData;
 using Discovery.Core.Model;
 using Prism.Commands;
@@ -6,13 +7,14 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Discovery.Client.DiscovererHomePage.ViewModels
 {
-    public class OtherUsersHomePageViewModel : BindableBase, INavigationAware
+    public class OtherUsersHomePageViewModel : BindableBase, INavigationAware, IRegionMemberLifetime
     {
         private Discoverer _discoverer;
         public Discoverer Discoverer
@@ -21,31 +23,96 @@ namespace Discovery.Client.DiscovererHomePage.ViewModels
             set => SetProperty(ref _discoverer, value);
         }
         private readonly IRegionManager _regionManager;
+        private ObservableCollection<Post> _postedPost;
+        public ObservableCollection<Post> PostedPosts
+        {
+            get => _postedPost;
+            set => SetProperty(ref _postedPost, value);
+        }
+        private ObservableCollection<Discoverer> _funs;
+        public ObservableCollection<Discoverer> Funs
+        {
+            get => _funs;
+            set => SetProperty(ref _funs, value);
+        }
+        private ObservableCollection<Post> _favoritedPost;
+        public ObservableCollection<Post> FavoritedPosts
+        {
+            get => _favoritedPost;
+            set => SetProperty(ref _favoritedPost, value);
+        }
+        private ObservableCollection<Discoverer> _idols;
+        public ObservableCollection<Discoverer> Idols
+        {
+            get => _idols;
+            set => SetProperty(ref _idols, value);
+        }
         public OtherUsersHomePageViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;
+            ViewThisUsersHomePageCommand = new DelegateCommand<Discoverer>(ViewThisUsersHomePage);
+            ViewPostDetailCommand = new DelegateCommand<Post>(ViewPostDetail);
             NavigationToProfileContentRegionCommand =
                 new DelegateCommand<string>(NavigationToProfileContentRegion);
         }
         public DelegateCommand<string> NavigationToProfileContentRegionCommand { get; }
-
         private void NavigationToProfileContentRegion(string viewName)
             => _regionManager.RequestNavigate(
-                                  RegionNames.OtherUsersProfileContent, 
+                                  RegionNames.OtherUsersProfileContent,
                                   viewName,
                                   new NavigationParameters
                                   {
                                       { "Discoverer", Discoverer }
                                   });
 
+        public DelegateCommand<Discoverer> ViewThisUsersHomePageCommand { get; }
+        private void ViewThisUsersHomePage(Discoverer discoverer)
+            => _regionManager.RequestNavigate(
+                                  RegionNames.MainMenuContent,
+                                  ViewNames.OtherUsersHomePage,
+                                  new NavigationParameters
+                                  {
+                                        { "Discoverer", discoverer }
+                                  });
+        public DelegateCommand<Post> ViewPostDetailCommand { get; }
+        public bool KeepAlive => false;
+
+        private void ViewPostDetail(Post post)
+            => _regionManager.RequestNavigate(
+                                  RegionNames.MainMenuContent,
+                                  ViewNames.OtherUsersPostDetail,
+                                  new NavigationParameters
+                                  {
+                                      { "Post", post }
+                                  });
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
             if (navigationContext.Parameters["Discoverer"] is Discoverer discoverer)
             {
                 Discoverer = discoverer;
+                LoadData();
             }
         }
+        private void LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                PostedPosts = new ObservableCollection<Post>(
+                                  databaseService.GetPostsOfTheDiscoverer(
+                                      Discoverer.BasicInfo.ID));
 
+                Funs = new ObservableCollection<Discoverer>(
+                           databaseService.GetFunsOfTheIdol(
+                               Discoverer.BasicInfo.ID));
+
+                FavoritedPosts = new ObservableCollection<Post>(
+                                     databaseService.GetFavoritePosts(
+                                         Discoverer.BasicInfo.ID));
+                Idols = new ObservableCollection<Discoverer>(
+                            databaseService.GetIdols(
+                                Discoverer.BasicInfo.ID));
+            }
+        }
         public bool IsNavigationTarget(NavigationContext navigationContext)
             => true;
 
