587793d40592b5bb83f642bf533954a1cc067070
ToBinary <ToBinary@outlook.com>
2019-05-27 14:24:27 +0800

    添加Discovery.Client.Search 模块的基本文件

---
+++ SRC/Client/Modules/Discovery.Client.Search/SearchModule.cs
@@ -0,0 +1,24 @@
+﻿using Discovery.Client.Search.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.Search
+{
+    public class SearchModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<GetSearchContent>();
+        }
+    }
+}
---
+++ SRC/Client/Modules/Discovery.Client.Search/ViewModels/GetSearchContentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Discovery.Core.Enums;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.Search.ViewModels
+{
+    public class GetSearchContentViewModel : BindableBase
+    {
+        
+
+    }
+}
