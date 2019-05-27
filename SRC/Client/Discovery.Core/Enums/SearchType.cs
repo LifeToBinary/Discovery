0c0fc1804ab8ce8b453608ec7fae53adbe1b8cc5
ToBinary <ToBinary@outlook.com>
2019-05-27 14:17:40 +0800

    添加 SearchType 枚举类型, 用于表示 搜索用户还是搜索帖子

---
+++ SRC/Client/Discovery.Core/Enums/SearchType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discovery.Core.Enums
+{
+    /// <summary>
+    /// 表示搜索的内容类型
+    /// </summary>
+    public enum SearchType
+    {
+        /// <summary>
+        /// 搜索用户
+        /// </summary>
+        SearchUser,
+        /// <summary>
+        /// 搜索帖子
+        /// </summary>
+        SearchPost
+    }
+}
