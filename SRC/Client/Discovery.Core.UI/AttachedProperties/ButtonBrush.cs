5c233f042219485391fcae3d0890be566cf0227f
ToBinary <ToBinary@outlook.com>
2019-05-31 01:50:05 +0800

    UI: 注册界面

---
+++ SRC/Client/Discovery.Core.UI/AttachedProperties/ButtonBrush.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Discovery.Core.UI.AttachedProperties
+{
+    public class ButtonBrush
+    {
+        public static readonly DependencyProperty ButtonPressBackgroundProperty =
+            DependencyProperty.RegisterAttached(
+                "ButtonPressBackground",
+                typeof(Brush),
+                typeof(ButtonBrush),
+                new PropertyMetadata(default(Brush)));
+        public static void SetButtonPressBackground(
+            DependencyObject element,
+            Brush value)
+            => element.SetValue(ButtonPressBackgroundProperty, value);
+        public static Brush GetButtonPressBackground(
+            DependencyObject element)
+            => element.GetValue(ButtonPressBackgroundProperty) as Brush;
+    }
+}
