1910d6fc490e15af4160f1944825775f0d4455a6
ToBinary <ToBinary@outlook.com>
2019-06-03 03:41:04 +0800

    添加了 Sex 枚举到Icon的转换器

---
+++ SRC/Client/Discovery.Core.UI/Converters/GenderToIconConverter.cs
@@ -0,0 +1,35 @@
+﻿using Discovery.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Discovery.Core.UI.Converters
+{
+    [ValueConversion(typeof(Sex), typeof(BitmapImage))]
+    public class GenderToIconConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+            => new[]
+            {
+                new BitmapImage(new Uri("http://47.240.12.27:10003/Discovery/DiscoveryWebFiles/Default/Images/MaleIcon.png")),
+                new BitmapImage(new Uri("http://47.240.12.27:10003/Discovery/DiscoveryWebFiles/Default/Images/FemaleIcon.png")),
+                null
+            }[(int)(Sex)value];
+
+        public object ConvertBack(
+            object value, 
+            Type targetType, 
+            object parameter, 
+            CultureInfo culture) 
+            => throw new NotSupportedException();
+    }
+}
