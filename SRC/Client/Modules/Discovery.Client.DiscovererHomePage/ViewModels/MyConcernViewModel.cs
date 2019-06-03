cd0b892392fff43007f2adc0ffbfc68317cac8c6
ToBinary <ToBinary@outlook.com>
2019-06-04 07:19:34 +0800

    UI: 当前用户的收藏/当前用户关注的人列表界面

---
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyConcernViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.Constants;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.DiscovererHomePage.ViewModels
+{
+    public class MyConcernViewModel : BindableBase, IRegionMemberLifetime
+    {
+        private readonly IRegionManager _regionManager;
+        private ObservableCollection<Discoverer> _myConcerns;
+        public ObservableCollection<Discoverer> MyConcerns
+        {
+            get => _myConcerns;
+            set => SetProperty(ref _myConcerns, value);
+        }
+        public MyConcernViewModel(IRegionManager regionManager)
+        {
+            MyConcerns = new ObservableCollection<Discoverer>();
+            _regionManager = regionManager;
+            ViewThisUsersHomePageCommand = new DelegateCommand<Discoverer>(
+                ViewThisUsersHomePage);
+            CancelConcernCommand = new DelegateCommand<Discoverer>(CancelConcern);
+            LoadData();
+        }
+        private async void LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                foreach (Discoverer discoverer in await databaseService.GetIdolsAsync(
+                    GlobalObjectHolder.CurrentUser.BasicInfo.ID))
+                {
+                    MyConcerns.Add(discoverer);
+                };
+            }
+        }
+        public DelegateCommand<Discoverer> CancelConcernCommand { get; }
+        private void CancelConcern(Discoverer discoverer)
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                databaseService.CancelConcern(
+                    GlobalObjectHolder.CurrentUser.BasicInfo.ID,
+                    discoverer.BasicInfo.ID);
+            }
+            MyConcerns.Remove(discoverer);
+        }
+        public DelegateCommand<Discoverer> ViewThisUsersHomePageCommand { get; }
+        public bool KeepAlive => false;
+
+        private void ViewThisUsersHomePage(Discoverer discoverer)
+            => _regionManager.RequestNavigate(
+                RegionNames.MainMenuContent,
+                ViewNames.OtherUsersHomePage,
+                new NavigationParameters
+                {
+                    { "Discoverer", discoverer}
+                });
+    }
+}
---
+++ SRC/Client/Modules/Discovery.Client.DiscovererHomePage/ViewModels/MyFavoritedPostsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Discovery.Client.DiscovererHomePage.DataBaseService;
+using Discovery.Core.Constants;
+using Discovery.Core.GlobalData;
+using Discovery.Core.Model;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discovery.Client.DiscovererHomePage.ViewModels
+{
+    public class MyFavoritedPostsViewModel : BindableBase, IRegionMemberLifetime
+    {
+        private readonly IRegionManager _regionManager;
+        private ObservableCollection<Post> _myFavoritedPosts;
+        public ObservableCollection<Post> MyFavoritedPosts
+        {
+            get => _myFavoritedPosts;
+            set => SetProperty(ref _myFavoritedPosts, value);
+        }
+
+        public MyFavoritedPostsViewModel(IRegionManager regionManager)
+        {
+            MyFavoritedPosts = new ObservableCollection<Post>();
+            _regionManager = regionManager;
+            ViewThisPostDetailCommand = new DelegateCommand<Post>(ViewThisPostDetail);
+            LoadData();
+        }
+        private async void LoadData()
+        {
+            using (var databaseService = new DataBaseServiceClient())
+            {
+                foreach (Post post in await databaseService.GetFavoritePostsAsync(
+                    GlobalObjectHolder.CurrentUser.BasicInfo.ID))
+                {
+                    MyFavoritedPosts.Add(post);
+                }
+            }
+        }
+        public DelegateCommand<Post> ViewThisPostDetailCommand { get; }
+        private void ViewThisPostDetail(Post post)
+            => _regionManager.RequestNavigate(
+                RegionNames.MainMenuContent,
+                ViewNames.OtherUsersPostDetail,
+                new NavigationParameters
+                {
+                    { "Post", post}
+                });
+        public bool KeepAlive => false;
+    }
+}
