3dacb37a7440a25f27feb2e465c5d69907acbac4
ToBinary <ToBinary@outlook.com>
2019-06-05 06:32:08 +0800

    向Discovery.Recommend项目的现有文件添加了注释

--- SRC/Client/Modules/Discovery.Client.Recommended/ViewModels/RecommendedViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.Recommended/ViewModels/RecommendedViewModel.cs
@@ -12,13 +12,25 @@ namespace Discovery.Client.Recommended.ViewModels
 {
     public class RecommendedViewModel : BindableBase
     {
+        /// <summary>
+        /// Region 导航对象
+        /// </summary>
         private readonly IRegionManager _regionManager;
+
+        /// <summary>
+        /// 推荐的帖子
+        /// </summary>
         private ObservableCollection<Post> _recommendedPosts;
         public ObservableCollection<Post> RecommendedPosts
         {
             get => _recommendedPosts;
             set => SetProperty(ref _recommendedPosts, value);
         }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="regionManager"></param>
         public RecommendedViewModel(IRegionManager regionManager)
         {
             _recommendedPosts = new ObservableCollection<Post>();
@@ -29,18 +41,28 @@ namespace Discovery.Client.Recommended.ViewModels
             ReloadDataCommand = new DelegateCommand(LoadData);
             AddNewPostCommand = new DelegateCommand(AddNewPost);
         }
+
+        /// <summary>
+        /// 发布帖子
+        /// </summary>
         public DelegateCommand AddNewPostCommand { get; }
         private void AddNewPost()
             => _regionManager.RequestNavigate(
                 RegionNames.MainMenuContent,
                 ViewNames.NewPost);
 
+        /// <summary>
+        /// 导航到搜索界面
+        /// </summary>
         public DelegateCommand NavigateToSearchViewCommand { get; }
         private void NavigateToSearchView()
             => _regionManager.RequestNavigate(
                 RegionNames.MainMenuContent,
                 ViewNames.GetSearchContent);
 
+        /// <summary>
+        /// 查看帖子
+        /// </summary>
         public DelegateCommand<Post> ViewPostDetailCommand { get; }
         private void ViewPostDetail(Post post)
             => _regionManager.RequestNavigate(
@@ -51,6 +73,9 @@ namespace Discovery.Client.Recommended.ViewModels
                     { "Post", post }
                 });
 
+        /// <summary>
+        /// 刷新推荐内容
+        /// </summary>
         public DelegateCommand ReloadDataCommand { get; }
         private async void LoadData()
         {
