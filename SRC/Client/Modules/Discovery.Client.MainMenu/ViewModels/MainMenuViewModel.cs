e396951570883a33121d37719e736fec693df45e
ToBinary <ToBinary@outlook.com>
2019-06-05 07:07:07 +0800

    向Discovery.Client项目的现有文件添加了注释

--- SRC/Client/Modules/Discovery.Client.MainMenu/ViewModels/MainMenuViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.MainMenu/ViewModels/MainMenuViewModel.cs
@@ -4,18 +4,25 @@ using Discovery.Core.Model;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Discovery.Client.MainMenu.ViewModels
 {
     public class MainMenuViewModel : BindableBase, IRegionMemberLifetime
     {
+        /// <summary>
+        /// 当前用户
+        /// </summary>
         public Discoverer CurrentUser { get; }
+
+        /// <summary>
+        /// Region 导航对象
+        /// </summary>
         private readonly IRegionManager _regionManager;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="regionManager"></param>
         public MainMenuViewModel(IRegionManager regionManager)
         {
             CurrentUser = GlobalObjectHolder.CurrentUser;
@@ -24,20 +31,25 @@ namespace Discovery.Client.MainMenu.ViewModels
                 new DelegateCommand<string>(NavigationViewToMainMenuContentRegion);
             NavigationViewToMainRegionCommand =
                 new DelegateCommand<string>(NavigationViewToMainRegion);
-            AddNewPostCommand = new DelegateCommand(AddNewPost);
         }
 
-        public DelegateCommand AddNewPostCommand { get; }
-        private void AddNewPost()
-            => _regionManager.RequestNavigate(RegionNames.MainMenuContent, ViewNames.NewPost);
+        /// <summary>
+        /// 导航视图到 MainMenuContent Region 中
+        /// </summary>
         public DelegateCommand<string> NavigationToMainMenuContentRegionCommand { get; }
         private void NavigationViewToMainMenuContentRegion(string viewName)
             => _regionManager.RequestNavigate(RegionNames.MainMenuContent, viewName);
 
+        /// <summary>
+        /// 导航视图到 MainRegion 中
+        /// </summary>
         public DelegateCommand<string> NavigationViewToMainRegionCommand { get; }
-        public bool KeepAlive => false;
-
         private void NavigationViewToMainRegion(string viewName)
             => _regionManager.RequestNavigate(RegionNames.MainRegion, viewName);
+
+        /// <summary>
+        /// 从 Region 离开时, 不保留此视图
+        /// </summary>
+        public bool KeepAlive => false;
     }
 }
