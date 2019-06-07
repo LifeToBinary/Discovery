56aff1fe0ef9bb809a6d3497b167e6931075fa9d
ToBinary <ToBinary@outlook.com>
2019-06-08 03:34:48 +0800

    为现有控件样式提供了DarkMagenta(暗紫色)版本

---
+++ SRC/Client/Discovery.Client.Resource/Themes/DarkMagentaTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Discovery.Client.Resource.Themes
+{
+    public partial class DarkMagentaTheme : ResourceDictionary
+    {
+        public DarkMagentaTheme() => InitializeComponent();
+    }
+}
