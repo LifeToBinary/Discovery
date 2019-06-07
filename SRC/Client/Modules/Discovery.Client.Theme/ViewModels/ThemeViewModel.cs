7778841280ed9f0382dbf5a2c317986ec4620f5f
ToBinary <ToBinary@outlook.com>
2019-06-08 06:18:09 +0800

    功能: 主题切换

--- SRC/Client/Modules/Discovery.Client.Theme/ViewModels/ThemeViewModel.cs
+++ SRC/Client/Modules/Discovery.Client.Theme/ViewModels/ThemeViewModel.cs
@@ -1,15 +1,38 @@
-﻿using Prism.Mvvm;
+﻿using Discovery.Client.Resource.Themes;
+using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Discovery.Client.Theme.ViewModels
 {
     public class ThemeViewModel : BindableBase, IRegionMemberLifetime
     {
         public bool KeepAlive => false;
+        public ThemeViewModel()
+        {
+            ChangeAppThemeCommand = new DelegateCommand<Core.Enums.Theme?>(ChangeAppTheme);
+        }
+        public DelegateCommand<Core.Enums.Theme?> ChangeAppThemeCommand { get; }
+        private void ChangeAppTheme(Core.Enums.Theme? newTheme)
+        {
+            if (newTheme == Core.Enums.Theme.DarkMagenta && 
+                Application.Current.Resources.MergedDictionaries[0].GetType() != typeof(DarkMagentaTheme))
+            {
+                Application.Current.Resources.MergedDictionaries[0] = new DarkMagentaTheme();
+                return;
+            }
+            if (newTheme == Core.Enums.Theme.Default &&
+                Application.Current.Resources.MergedDictionaries[0].GetType() != typeof(DefaultTheme))
+            {
+                Application.Current.Resources.MergedDictionaries[0] = new DefaultTheme();
+                return;
+            }
+        }
     }
 }
