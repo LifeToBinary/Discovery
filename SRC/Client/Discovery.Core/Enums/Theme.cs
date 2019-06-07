f0f5588047592d6d4ec1a02b5029ae020cd18742
ToBinary <ToBinary@outlook.com>
2019-06-08 05:45:38 +0800

    添加了 Theme 枚举类型

---
+++ SRC/Client/Discovery.Core/Enums/Theme.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discovery.Core.Enums
+{
+    /// <summary>
+    /// 表示一个主题
+    /// </summary>
+    public enum Theme
+    {
+        /// <summary>
+        /// 默认蓝色主题
+        /// </summary>
+        Default,
+        /// <summary>
+        /// 紫色主题
+        /// </summary>
+        DarkMagenta
+    }
+}
