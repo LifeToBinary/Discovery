8d8370d0e9234ee9186150229fd2bd6d831e4a59
ToBinary <ToBinary@outlook.com>
2019-06-10 18:49:19 +0800

    用户列表相关界面 对于当前用户项 使用特殊模板显示

---
+++ SRC/Client/Discovery.Core.UI/Selectors/UsersListDataTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using Discovery.Core.GlobalData;
+using Discovery.Core.RelationalModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Discovery.Core.UI.Selectors
+{
+    public class UsersListDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultDataTemplate { get; set; }
+        public DataTemplate CurrentUserDataTemplate { get; set; }
+        public override DataTemplate SelectTemplate(
+            object item,
+            DependencyObject _)
+            => (item as DiscovererRelationshipModel).Discoverer.BasicInfo.ID 
+                != GlobalObjectHolder.CurrentUser.BasicInfo.ID
+            ? DefaultDataTemplate
+            : CurrentUserDataTemplate;
+    }
+}
