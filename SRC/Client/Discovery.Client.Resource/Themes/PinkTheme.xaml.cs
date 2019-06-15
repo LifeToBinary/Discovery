1f454e766bcfa3ae17634c38cba0916a670809c3
ToBinary <ToBinary@outlook.com>
2019-06-15 17:11:32 +0800

    添加Pink粉红色主题资源

---
+++ SRC/Client/Discovery.Client.Resource/Themes/PinkTheme.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Discovery.Client.Resource.Themes
+{
+    public partial class PinkTheme : ResourceDictionary
+    {
+        private PinkTheme() => InitializeComponent();
+        public static PinkTheme Instance { get; } = new PinkTheme();
+    }
+}
